2e5e036b0ed1262da3b875211e531dca96289a7e
Nick Strupat <nick.strupat@gmail.com>
2025-05-03 19:40:57 -0400

    rewrite to check all arguments before executing the argument functions

--- trf.go
+++ trf.go
@@ -1,40 +1,62 @@
 package trf
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
+
+const (
+	bodyNilError            = "Argument `body` is nil. It must be a function."
+	recoverBlocksNilError   = "Argument `recovers` is nil. It must be a slice of RecoverBlock items."
+	recoverBlocksEmptyError = "Argument `recovers` is empty. It must contain at least one recover block."
+	recoverBlockNilError    = "Argument `recovers` contains a nil at index %d. All recover blocks must each be a function."
+)
 
 func Try(
 	body func(),
-	catches []RecoverBlock,
+	recoverBlocks []RecoverBlock,
 	finally func(),
 ) {
+	// Check the arguments
 	if body == nil {
-		panic("body argument is nil")
+		panic(bodyNilError)
 	}
+	if recoverBlocks == nil {
+		panic(recoverBlocksNilError)
+	}
+	if len(recoverBlocks) == 0 {
+		panic(recoverBlocksEmptyError)
+	}
+	for i, c := range recoverBlocks {
+		if c == nil {
+			panic(fmt.Sprintf(recoverBlockNilError, i))
+		}
+	}
+
+	// Defer the finally and recovery funcs
 	if finally != nil {
 		defer finally()
 	}
-	if catches != nil && len(catches) != 0 {
-		defer func() {
-			err := recover()
-			if err == nil {
+
+	defer func() {
+		err := recover()
+		if err == nil {
+			return
+		}
+		t := reflect.TypeOf(err)
+		for _, c := range recoverBlocks {
+			if c.tryInvoke(t, err) {
 				return
 			}
-			t := reflect.TypeOf(err)
-			for _, c := range catches {
-				if c == nil {
-					continue
-				}
-				if c.tryInvoke(t, err) {
-					return
-				}
-			}
-			panic(err)
-		}()
-	}
+		}
+		panic(err)
+	}()
+
+	// Call the body
 	body()
 }
 
-type Recovers []RecoverBlock
+type RecoverBlocks []RecoverBlock
 
 func Recover[TEx any](handler func(ex TEx)) RecoverBlock {
 	return recoverBlock[TEx]{
--- trf_test.go
+++ trf_test.go
@@ -1,8 +1,8 @@
-package trf_test
+package trf
 
 import (
+	"fmt"
 	"testing"
-	. "trf"
 )
 
 func TestTrf(t *testing.T) {
@@ -11,10 +11,68 @@ func TestTrf(t *testing.T) {
 			println("try body")
 			panic("panic")
 		},
-		Recovers{
+		RecoverBlocks{
 			Recover(func(err string) {
 				println("caught string: ", err)
 			}),
+			Recover(func(err int) {
+				println("caught int:", err)
+			}),
+			Recover(func(err any) {
+				println("caught any", err)
+			}),
+		},
+		func() {
+			println("finally")
+		},
+	)
+}
+
+func TestTrfNilBody(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic, got nil")
+		}
+	}()
+
+	Try(
+		nil,
+		RecoverBlocks{
+			Recover(func(err string) {
+				println("caught string: ", err)
+			}),
+			Recover(func(err int) {
+				println("caught int:", err)
+			}),
+			Recover(func(err any) {
+				println("caught any", err)
+			}),
+		},
+		func() {
+			println("finally")
+		},
+	)
+}
+
+func TestTrfNilRecoverBlock(t *testing.T) {
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("Expected panic, got nil")
+		}
+		msg := fmt.Sprintf(recoverBlockNilError, 0)
+		if r != msg {
+			t.Errorf("Expected panic with message '%v', got '%v'", msg, r)
+		}
+	}()
+
+	Try(
+		func() {
+			println("try body")
+			panic("panic")
+		},
+		RecoverBlocks{
+			nil,
 			Recover(func(err int) {
 				println("caught int:", err)
 			}),
